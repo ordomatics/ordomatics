b0f75316be91935fd6eba16ba7ce08d7e9b0be43
Moctar <moctarjallo@gmail.com>
2026-05-15 00:30:05 +0000

    feat: add ordomatics platform module

---
+++ ordomatics/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Ordomatics Platform",
+    "version": "18.0.1.0.0",
+    "category": "Technical",
+    "summary": "Platform-level dependencies for all Ordomatics deployments",
+    "license": "LGPL-3",
+    "depends": [
+        "fs_attachment_s3",
+        "whatsapp_llm",
+        "whatsapp_llm_payment",
+        "billing_llm",
+        "llm_mcp_server",
+    ],
+    "installable": True,
+    "auto_install": False,
+}
